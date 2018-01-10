92364f11ee0da25aebf181e641d85aa2a6b3a69b
liuxueyang <zypz457@sina.com>
2018-01-10 23:19:18 +0800

    update logging configuration

--- setup_logging.py
+++ setup_logging.py
@@ -1,8 +1,12 @@
 import logging.config
 import yaml
+import os
 
 
 def setup_logging():
+    if not os.path.exists('log'):
+        os.mkdir('log')
+
     with open('logging.yaml', 'r') as f:
         c = yaml.load(f.read())
         logging.config.dictConfig(c)
